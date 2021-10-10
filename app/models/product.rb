fac3ba8f880153572e6d1a9b26d096be2e10b55b
Karl Heitmann <khf1988@gmail.com>
2021-10-10 18:45:10 -0300

    anotados los atributos de los modelos

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :bigint           not null, primary key
+#  descripcion :text
+#  image_url   :string
+#  nombre      :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Product < ApplicationRecord
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :bigint           not null, primary key
+#  descripcion :text
+#  image_url   :string
+#  nombre      :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
